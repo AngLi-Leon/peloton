ecf5014613e6a8f3c33e3b006b31bac09aedd961
Yixin Luo <camellyx@gmail.com>
2017-05-30 15:34:09 -0400

    added test results

--- test/altertable/testAlterTable.py
+++ test/altertable/testAlterTable.py
@@ -26,6 +26,8 @@ def testAdd(options, psql_command, add_columns=1):
     os.system(psql_command + " -c \"alter table {} {};\" > /dev/null".format(
         options.table_name, commands))
     end = time.time()
+    commands = ", ".join(['drop column {}'.format(col)
+                          for col in added_column_names])
     os.system(psql_command +
               " -c \"drop table {};\" > /dev/null".format(options.table_name))
     return (end - start)
@@ -70,24 +72,6 @@ def main():
     peloton_command += " -p {}".format(options.port)
     genTable(options)
 
-    # Test add postgres
-    add_ts = []
-    for i in range(1, options.columns + 1):
-        ts = []
-        for _ in range(options.repeats):
-            os.system(postgres_command +
-                      " < {} > /dev/null".format(options.filename))
-            ts.append(testAdd(options, postgres_command, i))
-        add_ts.append([np.mean(ts), np.min(ts), np.max(ts)])
-    print(add_ts)
-
-    f, ax = plt.subplots(1)
-    add_ts = np.array(add_ts)
-    ax.plot(np.arange(options.columns) + 1,
-            add_ts.T[0], label='postgres')
-    ax.fill_between(np.arange(options.columns) + 1,
-                    add_ts.T[1], add_ts.T[2], alpha=.2)
-
     # Test add peloton
     add_ts = []
     for i in range(1, options.columns + 1):
@@ -99,9 +83,27 @@ def main():
         add_ts.append([np.mean(ts), np.min(ts), np.max(ts)])
     print(add_ts)
 
+    f, ax = plt.subplots(1)
     add_ts = np.array(add_ts)
     ax.plot(np.arange(options.columns) + 1,
             add_ts.T[0], label='peloton')
+    ax.fill_between(np.arange(options.columns) + 1,
+                    add_ts.T[1], add_ts.T[2], alpha=.2)
+
+    # Test add postgres
+    add_ts = []
+    for i in range(1, options.columns + 1):
+        ts = []
+        for _ in range(options.repeats):
+            os.system(postgres_command +
+                      " < {} > /dev/null".format(options.filename))
+            ts.append(testAdd(options, postgres_command, i))
+        add_ts.append([np.mean(ts), np.min(ts), np.max(ts)])
+    print(add_ts)
+
+    add_ts = np.array(add_ts)
+    ax.plot(np.arange(options.columns) + 1,
+            add_ts.T[0], label='postgres')
     ax.fill_between(np.arange(options.columns) + 1,
                     add_ts.T[1], add_ts.T[2], alpha=.2)
 
@@ -109,24 +111,6 @@ def main():
     ax.set_ylabel('Time (seconds)')
     ax.legend(framealpha=0)
     f.savefig('add.png')
-
-    # Test drop postgres
-    drop_ts = []
-    for i in range(1, options.columns + 1):
-        ts = []
-        for _ in range(options.repeats):
-            os.system(postgres_command +
-                      " < {} > /dev/null".format(options.filename))
-            ts.append(testDrop(options, postgres_command, i))
-        drop_ts.append([np.mean(ts), np.min(ts), np.max(ts)])
-    print(drop_ts)
-
-    f, ax = plt.subplots(1)
-    drop_ts = np.array(drop_ts)
-    ax.plot(np.arange(options.columns) + 1,
-            drop_ts.T[0], label='postgres')
-    ax.fill_between(np.arange(options.columns) + 1,
-                    drop_ts.T[1], drop_ts.T[2], alpha=.2)
 
     # Test drop peloton
     drop_ts = []
@@ -150,29 +134,6 @@ def main():
     ax.set_ylabel('Time (seconds)')
     ax.legend(framealpha=0)
     f.savefig('drop.png')
-
-    # Test tuples postgres
-    default_tuples = options.tuples
-    tuple_ts = []
-    for i in range(5):
-        options.tuples = default_tuples / 5 * (i + 1)
-        genTable(options)
-        ts = []
-        for _ in range(options.repeats):
-            os.system(postgres_command +
-                      " < {} > /dev/null".format(options.filename))
-            ts.append(testAdd(options, postgres_command, 1))
-        tuple_ts.append([np.mean(ts), np.min(ts), np.max(ts)])
-    print(tuple_ts)
-    options.tuples = default_tuples
-    genTable(options)
-
-    f, ax = plt.subplots(1)
-    tuple_ts = np.array(tuple_ts)
-    ax.plot(np.arange(5) * default_tuples / 1000,
-            tuple_ts.T[0], label='postgres')
-    ax.fill_between(np.arange(5) * default_tuples / 1000,
-                    tuple_ts.T[1], tuple_ts.T[2], alpha=.2)
 
     # Test tuples peloton
     default_tuples = options.tuples
@@ -202,28 +163,6 @@ def main():
     ax.legend(framealpha=0)
     f.savefig('tuples.png')
 
-    # Test columns postgres
-    default_columns = options.columns
-    column_ts = []
-    for i in range(5):
-        genTable(options)
-        ts = []
-        for _ in range(options.repeats):
-            os.system(postgres_command +
-                      " < {} > /dev/null".format(options.filename))
-            ts.append(testAdd(options, postgres_command, 1))
-        column_ts.append([np.mean(ts), np.min(ts), np.max(ts)])
-        options.columns += 1
-    print(column_ts)
-    options.columns = default_columns
-
-    f, ax = plt.subplots(1)
-    column_ts = np.array(column_ts)
-    ax.plot(np.arange(5) + default_columns,
-            column_ts.T[0], label='postgres')
-    ax.fill_between(np.arange(5) + default_columns,
-                    column_ts.T[1], column_ts.T[2], alpha=.2)
-
     # Test columns peloton
     default_columns = options.columns
     column_ts = []
@@ -235,8 +174,8 @@ def main():
                       " < {} > /dev/null".format(options.filename))
             ts.append(testAdd(options, peloton_command, 1))
         column_ts.append([np.mean(ts), np.min(ts), np.max(ts)])
-        # print(column_ts)
         options.columns += 1
+    print(column_ts)
     options.columns = default_columns
 
     f, ax = plt.subplots(1)
